31ed992d0a1db1860eae0c636fcdde2c0eb095d3
theosagras <42276726+theosagras@users.noreply.github.com>
2021-12-06 23:59:48 +0200

    προσθήκη σχολίου και μέθοδοf

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		//προσθήκη σχολίου 
 		Shape myShape = new Shape();
 		GeometricShape gshape = new GeometricShape(myShape);
 		
@@ -10,8 +10,9 @@ public class Main {
 		f.setSize(600, 600);
 		f.setContentPane(myShape);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		myShape.draw(150, 150);					        //APPLICATION 1
+		f.setSize(150,150);
+		myShape.draw(150, 150);		
+		myshape.draw(200,200);			        //APPLICATION 1
 		f.setVisible(true);
 		
 												//APPLICATION 2
